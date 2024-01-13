a693ae915d0a237c226245ba63e2a3b59b253a27
jakansha2001 <jakansha2001@gmail.com>
2024-01-13 23:48:54 +0530

    refactor: :recycle: extract PostTile widget

--- lib/home.dart
+++ lib/home.dart
@@ -10,54 +10,70 @@ class Home extends StatelessWidget {
         title: const Text('PhotoApp'),
         backgroundColor: Colors.blue,
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
         child: Column(
           children: [
             // const SizedBox(
             //   height: 20,
             // ),
-            Container(
-              color: Colors.grey.shade400,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      CircleAvatar(
-                        child: ClipOval(
-                          child: Image.network(
-                            'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=626&ext=jpg&ga=GA1.1.1808443959.1701905040&semt=sph',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      const Text('Username'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Image.network(
-                    'https://images.pexels.com/photos/126271/pexels-photo-126271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-                  ),
-                  const ListTile(
-                    leading: Text('Description'),
-                    trailing: Icon(Icons.favorite),
-                  ),
-                ],
-              ),
+            PostTile(),
+            SizedBox(
+              height: 5,
             ),
+            PostTile(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class PostTile extends StatelessWidget {
+  const PostTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.shade400,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              CircleAvatar(
+                child: ClipOval(
+                  child: Image.network(
+                    'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=626&ext=jpg&ga=GA1.1.1808443959.1701905040&semt=sph',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              const Text('Username'),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Image.network(
+            'https://images.pexels.com/photos/126271/pexels-photo-126271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+          ),
+          const ListTile(
+            leading: Text('Description'),
+            trailing: Icon(Icons.favorite),
+          ),
+        ],
       ),
     );
   }
