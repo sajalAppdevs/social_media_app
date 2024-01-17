e4be291ea93343a395c966afe7fe85e29ce1fb4a
jakansha2001 <jakansha2001@gmail.com>
2024-01-17 23:48:22 +0530

    refactor: refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:social_media_app/home.dart';
+import 'package:social_media_app/features/home/home.dart';
 
 void main() {
   runApp(const MyApp());
